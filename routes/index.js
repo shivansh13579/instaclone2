297bec9b26d859b1e1aeeb6ead359e60c40e3ce7
shivansh13579 <shivansh0975@gmail.com>
2023-12-17 21:48:45 +0530

    create login and register user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
+const userModel = require('./users')
+const localStrategy = require('passport-local');
+passport.use(new localStrategy(userModel.authenticate()));
 
 router.get('/', function(req, res) {
   res.render('index', {footer: false});
@@ -27,6 +31,34 @@ router.get('/edit', function(req, res) {
 
 router.get('/upload', function(req, res) {
   res.render('upload', {footer: true});
+});
+
+router.push('/register',function(req,res){
+  const data = new userModel({
+    username:req.body.username,
+    name:req.body.name,
+    email:req.body.email,
+  });
+
+  userModel.register(data,req.body.passport)
+  .then(function(registereduser){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect('/profile');
+    })
+  })
+});
+
+router.push("/login",passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}),function(req,res,next){
+
+});
+
+router.get("/logout",function(req,res,next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/login');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/instagram");
 
-module.exports = router;
+const userModel = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  profileImage: String,
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user",userSchema);
